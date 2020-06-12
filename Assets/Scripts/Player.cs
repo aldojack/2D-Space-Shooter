aeee1daa55b5f2569f61e13113d8cade34c00e16
Aldo <60137518+aldojack@users.noreply.github.com>
2020-06-12 01:12:34 +0100

    Add player, player script and simple movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _playerSpeed = 20.0f;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(_horizontalInput, _verticalInput, 0);
+
+        transform.Translate(direction * _playerSpeed * Time.deltaTime);
+    }
+}
