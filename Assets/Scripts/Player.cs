9ec5e63e4879213ae88b9f0b865524f581904e17
Aldo <60137518+aldojack@users.noreply.github.com>
2020-06-12 13:44:48 +0100

    Added bondaries to player to restrict movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,11 +19,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(_horizontalInput, _verticalInput, 0);
 
+        //Allows player to move
         transform.Translate(direction * _playerSpeed * Time.deltaTime);
+
+
+        //Player can exit either side to wrap around
+        if (transform.position.x <= -11.30f)
+        {
+            transform.position = new Vector3(11.30f, transform.position.y, 0);
+        }
+
+        else if (transform.position.x >= 11.38f)
+        {
+            transform.position = new Vector3(-11.31f, transform.position.y, 0);
+        }
+
+        //Stops player leaving top or bottom of screen
+        if (transform.position.y >= 5.7f)
+        {
+            transform.position = new Vector3(transform.position.x, 5.7f, 0);
+        }
+
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
     }
 }
